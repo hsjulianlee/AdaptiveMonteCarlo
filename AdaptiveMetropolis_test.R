4a1a179a80aaff9cb75d4ce13682df3bf2c21652
vadim <vadim@dbfin.com>
2018-02-15 17:26:19 -0600

    Added testing + plotting.

---
+++ AdaptiveMetropolis_test.R
@@ -0,0 +1,17 @@
+source("AdaptiveMetropolis.R")
+
+d = 10
+N <- 10000
+b <- 0.05
+
+O = rep(0, d)
+M = matrix(rnorm(d * d, 0, 1), ncol = d)
+MM = M %*% t(M)
+
+targetNMM = function(x, M = O, S = MM) {
+  return(dnorm(x, M, S))
+}
+
+trajectory = AMetro(target = targetNMM, trajLength = N, x = O, beta = b, burnin = 0)
+
+plot(1:length(trajectory[,1]), trajectory[,1], "l")
