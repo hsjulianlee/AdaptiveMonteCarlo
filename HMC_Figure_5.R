f1877c1c62ed9b80df6d79deb2a0d4ce9ef39c03
vadim <vadim@dbfin.com>
2018-09-24 22:14:08 -0500

    Added some comments, and changed the code so that k=100 is used everywhere.

--- HMC_Figure_5.R
+++ HMC_Figure_5.R
@@ -1,15 +1,15 @@
 source("HMC.R")
 
 # HMC run
-L = 150
-N = 1000
-k = 100
+L = 150 # the number of steps in each proposal (T-segment) 
+N = 1000 # the number of T-segments 
+k = 100 # the dimension of vectors (d in the paper) 
 
-Sigma = diag(seq(0.01, 1, by = 0.01)^2)
+Sigma = diag(seq(0.01, k * 0.01, by = 0.01)^2)
 mu = rep(0, k)
 q = mvrnorm(n = 1, mu, Sigma)
 
-HMC_sample = matrix(nrow = N, ncol = 100)
+HMC_sample = matrix(nrow = N, ncol = k)
 epsilon = runif(1, 0.0104, 0.0156)
 HMC_sample[1, ] = HMC(U, grad_U, epsilon, L, q)
 for (i in 2:N) {
@@ -18,7 +18,7 @@ for (i in 2:N) {
 }
 
 # Figure 5.6
-plot(HMC_sample[, 100], pch = 16, ylim = c(-3, 3),
+plot(HMC_sample[, k], pch = 16, ylim = c(-3, 3),
      las = 1,
      main = "Hamiltonian Monte Carlo",
      xlab = "Iteration",
@@ -26,7 +26,7 @@ plot(HMC_sample[, 100], pch = 16, ylim = c(-3, 3),
 
 # Figure 5.7 means
 means <- apply(HMC_sample, 2, mean)
-plot(seq(0.01, 1, by = 0.01), means, pch = 16, ylim = c(-0.7, 0.7),
+plot(seq(0.01, k * 0.01, by = 0.01), means, pch = 16, ylim = c(-0.7, 0.7),
      las = 1,
      main = "Hamiltonian Monte Carlo",
      xlab = "Standard deviation of coordinate",
@@ -35,7 +35,7 @@ abline(0, 0)
 
 # Figure 5.7 standard deviations
 sdevs <- apply(HMC_sample, 2, sd)
-plot(seq(0.01, 1, by = 0.01), sdevs, pch = 16, ylim = c(0, 1.2), xlim = c(0, 1),
+plot(seq(0.01, k * 0.01, by = 0.01), sdevs, pch = 16, ylim = c(0, 1.2), xlim = c(0, 1),
      xaxs = "i", yaxs = "i", las = 1,
      main = "Hamiltonian Monte Carlo",
      xlab = "Standard deviation of coordinate",
